b3bb5eb71fac4a6657d0d21ba12d1f6c3135e178
00008883 <wiut.00008883@gmail.com>
2021-04-07 14:44:31 +0500

    Validation fixed

--- public/javascript/validation.js
+++ public/javascript/validation.js
@@ -3,6 +3,7 @@ function Validation(){
     var name = document.forms["userForm"]["Name"].value
     var email = document.forms["userForm"]["Email"].value
     var option = document.forms["userForm"]["options"].value
+    var file = document.forms["userForm"]['file'].value
     // New variable to validate email
     var  formatmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -26,6 +27,10 @@ function Validation(){
         alert("Choose one option in the option menu");
         return false;
     }
+    if (file == "") {
+      alert("Select a file");
+      return false;
+  }
 
     return true;
 
